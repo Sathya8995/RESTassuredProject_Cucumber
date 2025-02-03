d5f9a7e05cbf88c0df2a7abc223c49cfdf43aef9
sathya <sathya.95.krishh@gmail.com>
2025-02-03 00:06:54 -0600

    AddingTestrunner

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+        features = "src/test/java/features",
+        glue = {"stepdefinitions"},
+        plugin ={"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+)
+public class TestRunner {
+
+}
